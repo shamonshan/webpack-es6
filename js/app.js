515e256cbcfcce26d6e4ccf6ae73ea0805877a6b
shamon <shamon@accubits.com>
2018-08-31 16:33:39 +0530

    Fisrt commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let name = 'hello world';
+
+module.exports = {
+  run: function (params) {
+    console.log('run from library',params);
+  }
+};
+
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+ var webpack = require('webpack');
+ module.exports = {
+     entry: './js/app.js',
+     output: {
+         path: path.resolve(__dirname, 'build'),
+         filename: 'app.bundle.js',
+         libraryTarget: 'var',
+         library: 'EntryPoint'
+     },
+     module: {
+         rules: [
+             {
+                 test: /\.js$/,
+                 loader: 'babel-loader',
+                 query: {
+                     presets: ['es2015']
+                 }
+             }
+         ]
+     },
+     stats: {
+         colors: true
+     },
+     devtool: 'source-map'
+ };
